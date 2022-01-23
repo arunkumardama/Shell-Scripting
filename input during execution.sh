10c66464b67dfa7c75169d6f7986f895f8068b8f
ardama <damaarunkumar9@gmail.com>
2022-01-23 16:10:25 +0530

    read command

---
+++ input during execution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##input during execution
+
+read -p "Enter your Name: "name
+
+echo "My name is $name"
+
+read -p "Enter you Course Name: " course
+
+echo -p "Course name is $course"
